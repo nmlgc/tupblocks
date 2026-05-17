6f84e178881f7ec8a728194799ba24ce53bb2aa8
nmlgc <nmlgc@nmlgc.net>
2026-05-17 20:03:56 +0200

    [Unix] Implement .so linking
    Part of P0340, funded by Ember2528.

--- toolchain.unix.lua
+++ toolchain.unix.lua
@@ -69,6 +69,44 @@ function CONFIG:cxxm(module_fn)
 	return self:CXXMWithOutput(module_fn, tup.base(module_fn), false)
 end
 
+---@param version_major string Mandatory major version
+---@param version_minor string? Optional minor version
+function CONFIG:dll(inputs, name, version_major, version_minor)
+	local libname = ("lib" .. name)
+	local soname_suffix = (".so." .. version_major)
+	local ext = soname_suffix
+	if version_minor and (version_minor:len() > 0) then
+		ext = (ext .. "." .. version_minor)
+	end
+	return self:CommonL(inputs, libname, ext, function(vars, basename, inps)
+		local soname = (basename .. soname_suffix)
+		local so_flags = ConcatFlags({
+			'-fPIC', '-shared', ('-Wl,-soname,' .. soname), '-o "%o"',
+		})
+		local cmd = (CXX .. inps .. so_flags .. ' ' .. ConcatFlags(vars.lflags))
+
+		local LN_CMD = '^o^ ln -s "%s" "%%o"'
+		local real_fn = tup.rule(vars.linputs, cmd, vars.loutputs)[1]
+		local major_fn = (self.vars.bindir .. soname)
+		local link_fn = (self.vars.bindir .. basename .. ".so")
+		local extra_inputs = real_fn
+		extra_inputs += tup.rule(
+			{}, string.format(LN_CMD, tup.file(real_fn)), major_fn
+		)
+		extra_inputs += tup.rule(
+			{}, string.format(LN_CMD, tup.file(major_fn)), link_fn
+		)
+		return {
+			lflags = {
+				("-L" .. self.vars.bindir),
+				("-l" .. basename:sub(4)),
+				"-Wl,-rpath,'$ORIGIN'"
+			},
+			linputs = { extra_inputs = extra_inputs },
+		}
+	end)
+end
+
 function CONFIG:exe(inputs, name)
 	return self:CommonL(inputs, name, "", function(vars, _, inps)
 		-- Inputs must come first to work properly with `-Wl,--as-needed`.
